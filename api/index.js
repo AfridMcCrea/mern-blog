4dcce5a06131f910570f6613eeb718bd63b5da28
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-16 20:03:25 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose, { connect } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("mongodb connected");
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 ');
