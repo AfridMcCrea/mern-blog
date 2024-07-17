4eb5b5fc5d593bfbc99050ef2dbd8c661a9bffe8
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-17 19:40:45 +0530

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -88,7 +88,7 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/signin' className="text-blue-500">Sign In</Link>
+            <Link to='/sign-in' className="text-blue-500">Sign In</Link>
           </div>
           {
             errorMessage && (
