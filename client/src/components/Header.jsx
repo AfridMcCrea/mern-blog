b6ee6818d9616a22caff542a82ef9bb182685b57
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-19 03:33:16 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,15 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link , useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa'
-import { useSelector }from 'react-redux'
+import {FaMoon, FaSun} from 'react-icons/fa'
+import { useSelector , useDispatch }from 'react-redux'
+import { toogleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const { currentUser } = useSelector(state => state.user );
+    const {theme} = useSelector(state => state.theme);
   return (
     <Navbar className='border-b-2'>
         <Link className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,8 +29,8 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon/>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toogleTheme())}>
+                { theme == 'light' ? <FaSun/>  : <FaMoon/>}
             </Button>
             { currentUser ? (
                 <Dropdown
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore , combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
 import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
@@ -8,6 +9,7 @@ import persistStore from 'redux-persist/es/persistStore';
 
 const rootReducer  = combineReducers({
     user: userReducer,
+    theme: themeReducer,
 });
 
 const persistConfig = {
