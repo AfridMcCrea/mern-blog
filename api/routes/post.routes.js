9a792533e40cc036a4991975f0ddc90857c4b622
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-24 00:24:03 +0530

    create get posts api route

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controller/post.controller.js';
+import { create, getposts } from '../controller/post.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create' , verifyToken , create) 
+router.get('/getposts' , getposts)
+
 
 export default router;
