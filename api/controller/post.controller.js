b99b7d00746ce004a91a21fe42bbe3e754eca793
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-24 15:18:04 +0530

    add delete post functionality to the dashboard

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -65,3 +65,15 @@ export const getposts = async(req,res,next)=> {
     }
 };
 
+export const deletepost = async(req, res, next ) =>{
+  if(!req.user.isAdmin|| req.user.id !== req.params.userId){
+    return next(errorHandler(403 , 'You are not allowed to delete this post.'));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('This post is deleted');
+  } catch (error) {
+    next(error);
+  }
+
+};
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,5 +1,6 @@
-import { Table } from 'flowbite-react';
+import { Button, Modal, Table } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 
@@ -7,7 +8,8 @@ export default function DashPosts() {
   const {currentUser } = useSelector((state)=>state.user);
   const [userPosts , setUserPosts] = useState([]);
   const [showMore , setShowMore] = useState(true);
-  console.log(userPosts);
+  const [showModel , setShowModel] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] = useState('');
   
   useEffect(()=>{
       const fetchPosts = async () => {
@@ -38,12 +40,35 @@ export default function DashPosts() {
         setUserPosts((prev) => [...prev , ...data.posts]);
         if(data.posts.length < 9){
           setShowMore(false);
+          setPostIdToDelete(post._id);
         }
       }
     } catch (error) {
       console.log(error)
     }
   }
+
+  const handleDeletePost = async()=>{
+      setShowModel(false);
+      try {
+        const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}` , 
+        {
+          method: 'DELETE',
+        });
+
+        const data = await res.json();
+        if(!res.ok){
+          console.log(data.message);
+        }
+        else{
+          setUserPosts((prev) =>
+          prev.filter((post) => post._id !== postIdToDelete)
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+  };
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100  dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -76,7 +101,10 @@ export default function DashPosts() {
                 </Table.Cell>
                 <Table.Cell>{post.category}</Table.Cell>
                 <Table.Cell>
-                  <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                  <span onClick={()=>{
+                    setShowModel(true)
+                    setPostIdToDelete(post._id)
+                  }} className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
                 </Table.Cell>
                 <Table.Cell>
                   <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
@@ -95,6 +123,19 @@ export default function DashPosts() {
       ) : (
         <p>You have no posts yet.</p>
       ) }
+      <Modal show={showModel} onClose={()=> setShowModel(false)} popup size = 'md'>
+          <Modal.Header/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+              <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete this post ?</h3>
+              <div className="flex justify-center gap-3">
+                <Button onClick={handleDeletePost} color='failure'>Yes I'm sure.</Button>
+                <Button color='gray' onClick={()=> setShowModel(false)}>No, cancel.</Button>
+              </div>
+            </div>
+          </Modal.Body>
+      </Modal>
     </div>
   )
 }
