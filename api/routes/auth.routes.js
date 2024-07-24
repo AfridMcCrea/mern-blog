e173fa5fd3baec68dcf46551ec945d1a49171f87
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-25 00:32:57 +0530

    create get users api

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import {google, signup , signin } from '../controller/auth.controller.js';
 
+
+
 const router = express.Router();
 
 router.post('/signup' , signup);
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express' 
 import { test , updateUser , deleteUser , signout} from '../controller/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import {getUsers} from '../controller/user.controller.js'
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get('/test' , test);
 router.put('/update/:userId', verifyToken , updateUser); 
 router.delete('/delete/:userId', verifyToken , deleteUser);
 router.post('/signout' , signout);
+router.get('/getusers' , verifyToken , getUsers);
 
 export default router;
