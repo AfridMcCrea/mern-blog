03b5f645486953b1ecf96ef44adcacdea10cefff
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-25 18:04:35 +0530

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,31 @@
+import { Button } from "flowbite-react";
+import React from "react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to print your custom design ?</h2>
+        <p className="text-gray-500 my-2">
+          Check out Rui-Raiment a custom T-shirt distribution company with
+          segment best material.
+        </p>
+        <Button gradientDuoTone="purpleToPink" className="rounded-tl-xl rounded-bl-none">
+          <a
+            href="https://www.linkedin.com/company/103330222/admin/dashboard/"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            Rui Raiment
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0070/7032/files/how_20to_20start_20a_20clothing_20brand.png?v=1693935729"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
--- client/src/components/PostPage.jsx
+++ client/src/components/PostPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import CatLoading from "../Animations/Cat";
 import { Link } from "react-router-dom";
 import { Button } from "flowbite-react";
+import CallToAction from "./CallToAction";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -66,6 +67,9 @@ export default function PostPage() {
         </span>
       </div>
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content}}></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction/>
+      </div>
     </main>
   );
 }
