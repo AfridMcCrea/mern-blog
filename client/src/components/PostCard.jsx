c40fcd0f122f6ccd289bb6f0f5bafd84a233e59b
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-26 21:07:35 +0530

    add recent article section to the post page

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function PostCard({ post }) {
+  return (
+    <div className="group relative w-[360px] h-[350px] overflow-hidden border sm:w-[360px] shadow-lg hover:border-2 transition-all">
+      <Link to={`/post/${post.slug}`}>
+        <img
+          src={post.image}
+          alt="post cover"
+          className="h-[220px] w-[360px] object-cover group-hover:h-[180px] transition-all duration-300 z-20"
+        />
+      </Link>
+      <div className="p-3 flex flex-col  gap-1">
+        <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
+        <span className="text-sm italic">{post.category}</span>
+        <Link
+          to={`/post/${post.slug}`}
+          className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border  border-[#4285F4] text-[#4285F4] hover:bg-[#4285F4] hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2"
+        >
+          Read Article
+        </Link>
+      </div>
+    </div>
+  );
+}
--- client/src/components/PostPage.jsx
+++ client/src/components/PostPage.jsx
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 import { Button } from "flowbite-react";
 import CallToAction from "./CallToAction";
 import CommentSection from "./CommentSection";
+import PostCard from "./PostCard";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -35,6 +37,22 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading) {
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -67,11 +85,21 @@ export default function PostPage() {
           {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
-      <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content}}></div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
       <div className="max-w-4xl mx-auto w-full">
-        <CallToAction/>
+        <CallToAction />
       </div>
-      <CommentSection postId={post._id}/>
+      <CommentSection postId={post._id} />
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">More Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
